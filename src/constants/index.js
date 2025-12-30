3dc5cd1edf29f1c9ed390e697ba2d2d07d388d14
FATCHEHOUNAKIWALE <fatchehounakiwale@yahoo.com>
2025-12-30 10:36:48 -1200

    Replace Adrian with Michel and fix ESLint warnings

--- src/constants/index.js
+++ src/constants/index.js
@@ -218,7 +218,7 @@ const testimonials = [
     name: "Guy Hawkins",
     mentions: "@guyhawkins",
     review:
-      "Collaborating with Michel was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Michel was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Michel's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Michel is the ideal partner.",
     imgPath: "/images/client2.png",
   },
   {
